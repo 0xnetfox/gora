93b9f544573d380f42648edd4a834ccf7dbb28ac
netfox <say-hi@netfox.rip>
2023-07-26 00:33:09 +0200

    impl gora_read_file() to parse input file

--- src/main.c
+++ src/main.c
@@ -1,7 +1,75 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
 #include "parser.h"
 
+#define READ_MAX     16
+#define STREAM_CHUNK 512
+
+char *gora_read_file(char *filename) {
+    int fd = open(filename, O_RDONLY);
+
+    if (fd == -1)
+        return NULL;
+
+    size_t read_sz;
+    char   buff[READ_MAX + 1];
+
+    size_t stream_sz    = 0;
+    size_t stream_alloc = STREAM_CHUNK;
+    char  *stream       = calloc(STREAM_CHUNK, sizeof(char));
+
+    while ((read_sz = read(fd, buff, READ_MAX)) > 0) {
+        long unsigned int req_sz = stream_sz + read_sz;
+
+        if (req_sz >= stream_alloc) {
+            char *new_stream;
+
+            if((new_stream = realloc(stream, stream_alloc + STREAM_CHUNK)) == NULL) {
+                goto free_err;
+            }
+
+            stream_alloc += STREAM_CHUNK;
+            stream = new_stream;
+        }
+
+        strncpy(stream + stream_sz, buff, read_sz);
+        stream_sz = req_sz;
+    }
+
+    if (read_sz == -1)
+        goto free_err;
+
+    stream[stream_sz + 1] = '\0';
+
+    return stream;
+
+free_err:
+    free(stream);
+    return NULL;
+}
+
 int main(int argc, char **argv) {
-    printf("uwu\n");
-    return 0;
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <filename>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    char *stream;
+    if ((stream = gora_read_file(argv[1])) == NULL)
+        goto file_err;
+
+    printf("%s", stream);
+
+    free(stream);
+
+    return EXIT_SUCCESS;
+
+file_err:
+    perror("error: could not process input file");
+    return EXIT_FAILURE;
 }
