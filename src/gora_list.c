e4a2558b7b2f1137389d41b4be68fd3e75c66cc1
netfox <say-hi@netfox.rip>
2023-07-27 02:03:49 +0200

    add double-linked list based on wayland impl

---
+++ src/gora_list.c
@@ -0,0 +1,14 @@
+#include <stddef.h>
+#include "gora_list.h"
+
+void gora_list_init(struct gora_list *lst) {
+    lst->prev = lst;
+    lst->next = lst;
+}
+
+void gora_list_insert(struct gora_list *lst, struct gora_list *elm) {
+    elm->prev = lst;
+    elm->next = lst->next;
+    lst->next = elm;
+    elm->next->prev = elm;
+}
---
+++ src/gora_list.h
@@ -0,0 +1,30 @@
+#ifndef GORA_LIST
+#define GORA_LIST
+
+/**
+ * defines a double-linked structure and methods to interact with it.
+ *
+ * Largely inspired by wayland's wl_list.
+ * https://gitlab.freedesktop.org/wayland/wayland/-/blob/main/src/wayland-util.h
+ */
+
+struct gora_list {
+    struct gora_list *prev;
+    struct gora_list *next;
+};
+
+void gora_list_init(struct gora_list *lst);
+
+void gora_list_insert(struct gora_list *lst, struct gora_list *elm);
+
+#define gora_container_of(p_item, type, member) \
+    (__typeof__(type)) ((char *) (p_item) - offsetof(__typeof__(*type), member))
+
+#define gora_list_foreach_safe(pos, tmp, head, member)            \
+    for(pos = gora_container_of((head)->next, pos, member),       \
+        tmp = gora_container_of((pos)->member.next, tmp, member); \
+        &pos->member != (head);                                   \
+        pos = tmp,                                                \
+        tmp = gora_container_of(pos->member.next, tmp, member))
+
+#endif // GORA_LIST
