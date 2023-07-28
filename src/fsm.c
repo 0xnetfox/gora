a3df6283d7af23f15fc70897e1f483dcbfef8b1c
netfox <say-hi@netfox.rip>
2023-07-28 13:18:47 +0200

    impl fsm solving algorithm

--- src/fsm.c
+++ src/fsm.c
@@ -1,6 +1,58 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
 #include "fsm.h"
 
-bool verify(struct fsm *fsm) {
-    return true;
+bool is_valid(struct fsm *fsm, uint8_t state) {
+    int i; uint8_t f_state;
+    while ((f_state = fsm->f_states[i++]) != GORA_FSM_INVALID_STATE) {
+        if (f_state == state) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+uint8_t next_state(struct fsm *fsm, uint8_t state, char input) {
+    for (int i = 0; fsm->transitions[i].i_state != GORA_FSM_INVALID_STATE; i++) {
+        struct transition *t = &fsm->transitions[i];
+        
+        if (t->i_state != state)
+            continue;
+
+        size_t sym_len = strlen(t->syms);
+        for (int j = 0; j < sym_len; j++) {
+            if (t->syms[j] == input) {
+                return t->n_state;
+            }
+        }
+    }
+
+    return GORA_FSM_INVALID_STATE;
+}
+
+struct fsm_result
+solve(struct fsm *fsm, uint8_t state, char *stream, size_t stream_off) {
+    char input = stream[stream_off];
+
+    if (input == '\0')
+        goto ret;
+
+    uint8_t next = next_state(fsm, state, input);
+    if (next != GORA_FSM_INVALID_STATE)
+        return solve(fsm, next, stream, stream_off + 1);
+
+ret:
+    return (struct fsm_result) {
+        .state           = state,
+        .valid           = is_valid(fsm, state),
+        .stream_consumed = stream_off,
+    };
+}
+
+struct fsm_result
+gora_fsm_solve(struct fsm *fsm, char *stream) {
+    return solve(fsm, fsm->i_state, stream, 0);
 }
--- src/fsm.h
+++ src/fsm.h
@@ -1,8 +1,17 @@
 #ifndef GORA_FSM_H
 #define GORA_FSM_H
 
+#include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
+
+#define GORA_FSM_ALPH_DIGIT ((char *) "0123456789\0")
+#define GORA_FSM_INVLALID_TRANSITION      \
+    ({ .i_state = GORA_FSM_INVALID_STATE, \
+       .n_state = GORA_FSM_INVALID_STATE, \
+       .syms = NULL })
+
+#define GORA_FSM_INVALID_STATE 0
 
 struct fsm {
     uint8_t            i_state;
@@ -11,11 +20,17 @@ struct fsm {
 };
 
 struct transition {
-    char*   s;
-    uint8_t qi;
-    uint8_t qn;
+    char*   syms;
+    uint8_t i_state;
+    uint8_t n_state;
 };
 
-bool verify(struct fsm *fsm);
+struct fsm_result {
+    bool    valid;
+    uint8_t state;
+    size_t  stream_consumed;
+};
+
+struct fsm_result gora_fsm_solve(struct fsm *fsm, char *stream);
 
 #endif // GORA_FSM_H
